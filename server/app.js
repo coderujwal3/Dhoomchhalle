92dcb29caa81e9a1d4335f24e7f8ab8bd4c2c9f4
coderujwal3 <nikhilsengar7012@gmail.com>
2026-03-21 19:23:27 +0530

    add

--- server/app.js
+++ server/app.js
@@ -7,10 +7,11 @@ const app = express();
 
 app.use(cookieParser())
 app.use(express.json())
-app.use(cors({
-  origin: process.env.CLIENT_ORIGIN || "http://localhost:5173",
-  credentials: true
-}));
+// app.use(cors({
+//   origin: process.env.CLIENT_ORIGIN || "http://localhost:5173",
+//   credentials: true
+// }));
+app.use(cors());
 
 const userRoutes = require('./modules/user/user.routes')
 const hotelRoutes = require('./modules/hotel/hotel.routes')

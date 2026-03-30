f9f3dc0890241ff1a86c5631544eacc45fa86af9
coderujwal3 <nikhilsengar7012@gmail.com>
2026-03-30 14:54:10 +0530

    fixed: cors origin domains

--- server/app.js
+++ server/app.js
@@ -12,20 +12,8 @@ app.use(helmet());
 app.use(express.json({ limit: "5mb" }))
 app.use(express.urlencoded({ extended: true, limit: "5mb" }))
 
-const allowedOrigins = [
-  process.env.CLIENT_ORIGIN,
-  "http://localhost:5173",
-  "http://127.0.0.1:5173",
-].filter(Boolean);
-
 app.use(cors({
-  origin: (origin, callback) => {
-    if (!origin || allowedOrigins.includes(origin)) {
-      return callback(null, true);
-    }
-
-    return callback(new Error("CORS origin not allowed"));
-  },
+  origin: process.env.CLIENT_ORIGIN || "http://localhost:5173",
   credentials: true
 }));
 

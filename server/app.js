a0bfd3ac2a4f107077ad66123eebb6f5766c4cc6
coderujwal3 <nikhilsengar7012@gmail.com>
2026-04-14 23:10:35 +0530

    fixed: cors origin

--- server/app.js
+++ server/app.js
@@ -28,8 +28,8 @@ app.use(express.json({ limit: "5mb" }))
 app.use(express.urlencoded({ extended: true, limit: "5mb" }))
 
 app.use(cors({
-  // origin: process.env.CLIENT_ORIGIN || "http://localhost:5173",
-  origin: "http://localhost:5173",
+  origin: process.env.CLIENT_ORIGIN || "http://localhost:5173",
+  // origin: "http://localhost:5173",
   credentials: true
 }));
 

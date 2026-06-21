b2d60936cbf74cd3cd58631a1ed1fa35fdb7b980
coderujwal3 <nikhilsengar7012@gmail.com>
2026-06-21 11:54:19 +0530

    Fixed: cors origin urls

--- client/src/lib/apiClient.js
+++ client/src/lib/apiClient.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  // baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:3000/api/v1",
-  baseURL: "http://localhost:3000/api/v1",
+  baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:3000/api/v1",
+  // baseURL: "http://localhost:3000/api/v1",
   withCredentials: true,
 });
 
